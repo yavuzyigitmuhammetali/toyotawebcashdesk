f134cf039a7599617054c9b71ca6a083750732eb
yavuzyigitmuhammetali <yavuzyigitmuhammetali@gmail.com>
2024-02-03 01:51:20 +0300

    the issue of OnlineOfflineIndicator component taking up unnecessary space has been resolved

--- src/shared/componenets/OnlineOfflineIndicator.jsx
+++ src/shared/componenets/OnlineOfflineIndicator.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 
-function OnlineOfflineIndicator({online = false, circleSize = 5}) {
+function OnlineOfflineIndicator({online = false}) {
+    const circleSize = 5;
     return (
         <div style={{display:"inline-block",position: "relative",boxSizing:"border-box",paddingLeft:"10px"}}>
             <div style={online ? {color: "green"} : {color: "red"}}>{online ? "Online" : "Offline"}</div>
-            <div style={{height:"100px",position:"absolute",left:"0",top:"50%",transform:"translate(-50%, -50%)"}}>
-                <svg width="100" height="100">
-                    <circle cx="50" cy="50" r={circleSize} fill={online ? "green" : "red"} id="point"/>
-                    <circle cx="50" cy="50" r={circleSize} fill="none" stroke={online ? "green" : "red"}
+            <div style={{height:"20px",position:"absolute",left:"0",top:"50%",transform:"translate(-50%, -50%)"}}>
+                <svg width="20" height="20">
+                    <circle cx="10" cy="10" r={circleSize} fill={online ? "green" : "red"} id="point"/>
+                    <circle cx="10" cy="10" r={circleSize} fill="none" stroke={online ? "green" : "red"}
                             strokeWidth="2">
                         <animate attributeName="r" from={circleSize} to={circleSize * 2} dur="1s" begin="0s"
                                  repeatCount="indefinite"/>
