f7932216e64d7e7921bd1676afeb4bc75de5476c
yavuzyigitmuhammetali <yavuzyigitmuhammetali@gmail.com>
2024-04-19 01:16:14 +0300

    ProductsDashboard has been refactored

--- src/shared/components/ResponsiveDialog.jsx
+++ src/shared/components/ResponsiveDialog.jsx
@@ -6,7 +6,6 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import {useTheme} from '@mui/material/styles';
 
 export default function ResponsiveDialog({
                                              children,
