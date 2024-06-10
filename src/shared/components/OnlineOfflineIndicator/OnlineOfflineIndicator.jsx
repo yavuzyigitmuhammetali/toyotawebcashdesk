51dd656f04bd5f783774f8375fd799eeba88f0fd
yavuzyigitmuhammetali <yavuzyigitmuhammetali@gmail.com>
2024-06-11 01:00:09 +0300

    performance mode integration for the OnlineOfflineIndicator component has been completed

--- src/shared/components/OnlineOfflineIndicator/OnlineOfflineIndicator.jsx
+++ src/shared/components/OnlineOfflineIndicator/OnlineOfflineIndicator.jsx
@@ -1,7 +1,13 @@
+// Filename: OnlineOfflineIndicator.jsx
 import React from 'react';
 import './onlineOfflineIndicator.css';
 
-const OnlineOfflineIndicator = React.memo(({online = false, language = 'en', dark = false}) => {
+const OnlineOfflineIndicator = React.memo(({
+                                               online = false,
+                                               language = 'en',
+                                               dark = false,
+                                               performanceMode = false
+                                           }) => {
     const circleSize = 5;
     const languageText = {
         en: {online: "Online", offline: "Offline"},
@@ -14,15 +20,22 @@ const OnlineOfflineIndicator = React.memo(({online = false, language = 'en', dar
                 {online ? languageText[language].online : languageText[language].offline}
             </div>
             <div className="circle-container">
-                <svg width="20" height="20">
-                    <circle cx="10" cy="10" r={circleSize} fill={online ? "green" : "red"} id="point"/>
-                    <circle cx="10" cy="10" r={circleSize} fill="none" stroke={online ? "green" : "red"}
-                            strokeWidth="2">
-                        <animate attributeName="r" from={circleSize} to={circleSize * 2} dur="1s" begin="0s"
-                                 repeatCount="indefinite"/>
-                        <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0s" repeatCount="indefinite"/>
-                    </circle>
-                </svg>
+                {performanceMode ? (
+                    <svg width="20" height="20">
+                        <circle cx="10" cy="10" r={circleSize} fill={online ? "green" : "red"}/>
+                    </svg>
+                ) : (
+                    <svg width="20" height="20">
+                        <circle cx="10" cy="10" r={circleSize} fill={online ? "green" : "red"} id="point"/>
+                        <circle cx="10" cy="10" r={circleSize} fill="none" stroke={online ? "green" : "red"}
+                                strokeWidth="2">
+                            <animate attributeName="r" from={circleSize} to={circleSize * 2} dur="1s" begin="0s"
+                                     repeatCount="indefinite"/>
+                            <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0s"
+                                     repeatCount="indefinite"/>
+                        </circle>
+                    </svg>
+                )}
             </div>
         </div>
     );
