39f5a71150bc41303ac373d221f5a41e32b021f1
yavuzyigitmuhammetali <yavuzyigitmuhammetali@gmail.com>
2024-06-11 17:01:10 +0300

    performance mode integration for the ShoppingCartItem component has been completed

--- src/shared/components/ShoppingCartItem/ShoppingCartItem.jsx
+++ src/shared/components/ShoppingCartItem/ShoppingCartItem.jsx
@@ -1,12 +1,11 @@
 import React, {useState} from 'react';
-import "./shoppingCartItem.css"
+import "./shoppingCartItem.css";
 import {IconButton} from "@mui/material";
 import RemoveIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Remove';
 import {createTheme, ThemeProvider} from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
-
 
 const ShoppingCartItem = React.memo(({
                                          disabled = false,
@@ -27,7 +26,8 @@ const ShoppingCartItem = React.memo(({
                                          onFocus,
                                          decimalValue,
                                          id,
-                                         lang = "tr"
+                                         lang = "tr",
+                                         performanceMode = false
                                      }) => {
     const taxFreePrice = ((discountedPrice ? discountedPrice : price) / (1 + (tax / 100))).toFixed(2);
     const finalPrice = (quantity * price).toFixed(2);
@@ -40,55 +40,63 @@ const ShoppingCartItem = React.memo(({
         }
         return setElementVisibility(!isElementVisible)
     }
-    return (<div style={{backgroundColor: dark && "#12161B", color: dark && "white", borderColor: dark && "white"}}
-                 className={disabled ? "shopping-cart-item-container shopping-cart-item-disabled" : "shopping-cart-item-container"}>
-        <ThemeProvider theme={createTheme({palette: {mode: dark ? "dark" : "light"}})}>
-            <div onClick={toggleElementVisibility} className="shopping-cart-item-content-container">
-                <div className="shopping-cart-item-content">
-                    <span>#{barcode}</span>
-                    <span>{taxFreePrice}$ + KDV %{tax}</span>
-                    <span>{quantity} {fraction ? (lang === "tr" ? "Kilo" : "Kilo") : (lang === "tr" ? "Adet" : "Piece")}</span>
-                </div>
-                <div className="shopping-cart-item-content">
-                    <span className="product-name">{index}.{productName}</span>
-                    <div>{discountedPrice ? campaign : ""}</div>
-                    <span className="price">
-                        <span className={`original-price ${discountedPrice ? 'discounted' : ''}`}>
-                            {finalPrice}$
+    return (
+        <div
+            style={{backgroundColor: dark && "#12161B", color: dark && "white", borderColor: dark && "white"}}
+            className={`${disabled ? "shopping-cart-item-container shopping-cart-item-disabled" : "shopping-cart-item-container"} ${performanceMode ? "performance-mode" : ""}`}
+        >
+            <ThemeProvider theme={createTheme({palette: {mode: dark ? "dark" : "light"}})}>
+                <div onClick={toggleElementVisibility} className="shopping-cart-item-content-container">
+                    <div className="shopping-cart-item-content">
+                        <span>#{barcode}</span>
+                        <span>{taxFreePrice}$ + KDV %{tax}</span>
+                        <span>{quantity} {fraction ? (lang === "tr" ? "Kilo" : "Kilo") : (lang === "tr" ? "Adet" : "Piece")}</span>
+                    </div>
+                    <div className="shopping-cart-item-content">
+                        <span className="product-name">{index}.{productName}</span>
+                        <div>{discountedPrice ? campaign : ""}</div>
+                        <span className="price">
+                            <span className={`original-price ${discountedPrice ? 'discounted' : ''}`}>
+                                {finalPrice}$
+                            </span>
+                            {discountedPrice ?
+                                <span className="discounted-price"> {finalDiscountedPrice}$</span> : null}
                         </span>
-                        {discountedPrice ? <span className="discounted-price"> {finalDiscountedPrice}$</span> : null}
-                    </span>
+                    </div>
                 </div>
-            </div>
-            {firstLoad && <div
-                className={`shopping-cart-item-editor ${isElementVisible ? 'jell-in' : 'jell-out'} ${dark ? 'dark-bg' : ''}`}>
-                {fraction ? <TextField
-                    id={id}
-                    variant="standard"
-                    type="number"
-                    onFocus={onFocus}
-                    value={decimalValue}
-                    onChange={onChangeDecimal}
-                /> : <>
-                    <IconButton onClick={onAdd} color="success" size="small" aria-label="delete">
-                        <AddIcon/>
-                    </IconButton>
-                    <IconButton onClick={onDelete} color="warning" size="small" aria-label="delete">
-                        <DeleteIcon/>
-                    </IconButton>
-                </>}
+                {firstLoad && (
+                    <div
+                        className={`shopping-cart-item-editor ${isElementVisible && !performanceMode ? 'jell-in' : 'jell-out'} ${dark ? 'dark-bg' : ''}`}
+                        style={performanceMode ? {left: isElementVisible ? '0' : '-50%'} : {}}
+                    >
+                        {fraction ? (
+                            <TextField
+                                id={id}
+                                variant="standard"
+                                type="number"
+                                onFocus={onFocus}
+                                value={decimalValue}
+                                onChange={onChangeDecimal}
+                            />
+                        ) : (
+                            <>
+                                <IconButton onClick={onAdd} color="success" size="small" aria-label="delete">
+                                    <AddIcon/>
+                                </IconButton>
+                                <IconButton onClick={onDelete} color="warning" size="small" aria-label="delete">
+                                    <DeleteIcon/>
+                                </IconButton>
+                            </>
+                        )}
 
-                <IconButton onClick={onRemove} color="error" size="small" aria-label="delete">
-                    <RemoveIcon/>
-                </IconButton>
-            </div>}
-        </ThemeProvider>
-
-    </div>);
+                        <IconButton onClick={onRemove} color="error" size="small" aria-label="delete">
+                            <RemoveIcon/>
+                        </IconButton>
+                    </div>
+                )}
+            </ThemeProvider>
+        </div>
+    );
 });
 
 export default ShoppingCartItem;
-
-
-
-
